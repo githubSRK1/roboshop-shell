5d5c503f415af342d2c146d79bbe42d05af15040
SRK <RanjithSrikonda.Devops@example.com>
2023-08-26 12:21:20 +0530

    updated catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ useradd roboshop &>>$LOGFILE
 # write a condition to check directory already exists or not
 mkdir /app &>>$LOGFILE
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
 VALIDATE $? "Downloading catalogue artifact"
 
